eac89ac304edcc2fc318860b92afbc6694cf59e5
StepaKovacic <stepan.kovacic@gmail.com>
2026-07-18 20:09:58 +0200

    setup na frontend

--- app/main.py
+++ app/main.py
@@ -1,15 +1,19 @@
-from dbHandler import session 
+from dbHandler import session
 from models import User, Invoice, Item
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from uvicorn import run
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
+App = FastAPI()
+App.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-user = User(
-    username="stepanKralMatematiky",
-    email="stepan.kral.matematiky@example.com",
-    password_hash="Frantisek444",
-    first_name="Štěpán",
-    last_name="Noha",
-    ico="2243456",
-)
-session.add(user)
-session.commit() 
-print(user.id)
+@App.get("/", response_class = HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse(request = request, name="index.html")
+
---
+++ todo.md
@@ -0,0 +1,4 @@
+- docs
+- frontend rendering 
+- auth !!!
+- session management 
